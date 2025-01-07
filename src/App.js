6150e85c7c2c4ba1bbdf513926107d9b96e5562b
saryCast <cast.sara@gmail.com>
2025-01-07 11:47:44 -0300

    remplazando las funciones a clases POO

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Container, Row} from 'reactstrap';
 import './App.css';
 import Producto from './Componentes/Producto';
 
+/*
 function App() {
   return (
     <Container>
@@ -36,4 +37,41 @@ function App() {
   );
 }
 
+export default App;
+*/
+//Cambiamos a POO
+class App extends React.Component{
+    render(){
+    return (
+      <Container>
+        <Row>
+        <Producto
+          titulo="Headphone Purple"
+          imagen="https://image.freepik.com/vector-gratis/ilustracion-icono-auriculares_17146-29.jpg"
+          descripcion="Audifonos Alta calidad de sonido, HD Sonido Envolvente"
+          precio="24.390"
+        />
+        <Producto
+          titulo="Headphone Purple"
+          imagen="https://image.freepik.com/vector-gratis/ilustracion-icono-auriculares_17146-29.jpg"
+          descripcion="Audifonos Alta calidad de sonido, HD Sonido Envolvente"
+          precio="24.390"
+        />
+        <Producto
+          titulo="Headphone Purple"
+          imagen="https://image.freepik.com/vector-gratis/ilustracion-icono-auriculares_17146-29.jpg"
+          descripcion="Audifonos Alta calidad de sonido, HD Sonido Envolvente"
+          precio="24.390"
+        />
+        <Producto
+          titulo="Headphone Purple"
+          imagen="https://image.freepik.com/vector-gratis/ilustracion-icono-auriculares_17146-29.jpg"
+          descripcion="Audifonos Alta calidad de sonido, HD Sonido Envolvente"
+          precio="24.390"
+        />
+        </Row>
+      </Container>
+    );
+  }
+}
 export default App;
--- src/Componentes/Producto.js
+++ src/Componentes/Producto.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Button, CardText, Col,CardSubtitle, CardImg,CardTitle,CardBody, Card } from 'reactstrap';
 import './Producto.css'
 
-
+/*
 function Producto(props){
     return(
         <Col sm="4">
@@ -20,4 +20,28 @@ function Producto(props){
         </Col>
     );
 }
+export default Producto;
+*/
+
+// En vez de funcion la pasamos a una clase para hacer POO
+class Producto extends React.Component{
+    render(){
+        return(
+            <Col sm="4">
+                <Card className="Card" body outline color='primary'>
+                    <CardImg src={this.props.imagen}/>
+                    <CardBody>
+                        <CardTitle>{this.props.titulo}</CardTitle>
+                        <CardSubtitle><b>Valor : </b>{this.props.precio}</CardSubtitle>
+                        <CardText>{this.props.descripcion}
+                        </CardText>
+                        <Button className='Button'>Comprar</Button>
+                        <Button className='Button'>Ver Ficha</Button>
+                    </CardBody>
+                </Card>
+            </Col>
+        );
+    }
+}
+
 export default Producto;
